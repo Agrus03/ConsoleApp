d1690493390a3d0efe7743dd7f8ddf9f84fb3337
vitalij <mommovis@gmail.com>
2026-02-22 22:13:32 +0200

    update Models.cs

--- Models.cs
+++ Models.cs
@@ -4,20 +4,10 @@ namespace ConsoleApp
 {
     public class Wallet
     {
-        public string WalletId { get; set; }
-        public Owner Owner { get; set; }
+        public string? WalletId { get; set; }
         public Balance Balance { get; set; }
         
     }
-
-    public class Owner
-    {
-        public int UserId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public bool IsVerified { get; set; }
-    }
-
     public class Balance
     {
         public string Currency { get; set; }
