4049f8ece4f2c479ec9aee7d6646dfeee1959fe4
vitalij <mommovis@gmail.com>
2026-02-22 23:03:00 +0200

    update Models.cs

--- Models.cs
+++ Models.cs
@@ -14,4 +14,21 @@ namespace ConsoleApp
         public decimal Available { get; set; }
         public decimal Blocked { get; set; }
     }
+
+    public class Transaction
+    {
+        public string TransactionId { get; set; }
+        public string Type { get; set; }
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+        public string Status { get; set; }
+        public string Timestamp { get; set; }
+        public Metadata Metadata { get; set; } 
+    }
+    
+    public class Metadata
+    {
+        public string Source { get; set; }
+        public string Reference { get; set; }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-
+﻿using System;
+using System.IO;
 namespace ConsoleApp
 {
     class Program
     {
         static void Main()
-        {}
+        {
+            string filePath = "TransactionId.txt";
+            string[] lines = File.ReadAllLines(filePath);
+            Wallet wallet = new Wallet();
+            wallet.Balance = new Balance();
+            string currentContext = "root";
+            Transaction currentTransaction = null;
+        }
     }
 }
